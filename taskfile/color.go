2ad95640ea966e26d0e1e67c41435c907a2ce394
David Gageot <david.gageot@docker.com>
2026-04-09 14:54:40 +0200

    refactor: remove unused colorRed constant
    Assisted-By: docker-agent

--- taskfile/color.go
+++ taskfile/color.go
@@ -8,7 +8,6 @@ import (
 // ANSI color escape sequences.
 const (
 	colorReset  = "\033[0m"
-	colorRed    = "\033[31m"
 	colorGreen  = "\033[32m"
 	colorYellow = "\033[33m"
 	colorCyan   = "\033[36m"
