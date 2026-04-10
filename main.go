ece094d7076e5047c8afe839ed01eb92f0295b14
David Gageot <david.gageot@docker.com>
2026-04-10 08:18:59 +0200

    Simplify secretSet by removing intermediate variables
    Assisted-By: docker-agent

--- main.go
+++ main.go
@@ -132,12 +132,10 @@ func secretSet(args []string) error {
 		return errors.New("usage: gogo secret set <service> <key> <value>")
 	}
 
-	service, key, value := args[0], args[1], args[2]
-
-	if err := taskfile.SetSecret(service, key, value); err != nil {
+	if err := taskfile.SetSecret(args[0], args[1], args[2]); err != nil {
 		return err
 	}
 
-	fmt.Fprintf(os.Stderr, "Secret %q stored in keychain %q\n", key, service)
+	fmt.Fprintf(os.Stderr, "Secret %q stored in keychain %q\n", args[1], args[0])
 	return nil
 }
